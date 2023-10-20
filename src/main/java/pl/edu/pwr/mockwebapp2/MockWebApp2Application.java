fe95d62330b3f37fde88cc1834eabf7fd32d1b21
Onur Elhakan <72891423+oelhakan@users.noreply.github.com>
2023-10-20 10:41:40 +0200

    CRUD

--- src/main/java/pl/edu/pwr/mockwebapp2/MockWebApp2Application.java
+++ src/main/java/pl/edu/pwr/mockwebapp2/MockWebApp2Application.java
@@ -3,10 +3,7 @@ package pl.edu.pwr.mockwebapp2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.pwr.mockwebapp2.model.Employee;
 import pl.edu.pwr.mockwebapp2.repository.EmployeeRepository;
 
@@ -22,6 +19,17 @@ public class MockWebApp2Application {
     @PostMapping("/employees/add")
     public Employee addEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/remove/{employeeId}")
+    public int removeEmployee(@PathVariable(value="employeeId") int employeeId){
+        employeeRepository.deleteById(employeeId);
+        return employeeId;
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable(value="employeeId") int employeeId){
+        return employeeRepository.findById(employeeId).isPresent() ? employeeRepository.findById(employeeId).get() : null;
     }
 
     @GetMapping("/employees")
